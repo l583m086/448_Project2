a90ff0b88dfbc5b4bc94b52a51a86703f244e570
Will Thomas <30wthomas@gmail.com>
2020-09-12 15:37:48 -0500

    Space.js documentation

--- public/space.js
+++ public/space.js
@@ -1,3 +1,4 @@
+// Possible state values for Space object
 const states = {
     EMPTY: "Empty",
     PLACED: "Ship",
@@ -6,13 +7,30 @@ const states = {
     SUNK: "Sunk"
 }
 
+/*
+* Class: Space
+* Pre: None
+* Post: new Space object returned
+*/
 export default class Space {
+    /*
+    * Class: Space
+    * Pre: None
+    * Params: 'x': x-coordinate; 'y': y-coordinate;
+    * Post: New Space object returned, default m_state set to empty
+    */
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.m_state = states.EMPTY;
     }
 
+    /*
+    * Method: coordinate
+    * Pre: Space exists, called with .coordinate
+    * Params: None
+    * Post: Getter for the values of the x and y coordinates on this Space object. Returned as {x: this.x, y: this.y}
+    */
     get coordinate() {
         return (
             {
@@ -22,10 +40,22 @@ export default class Space {
         )
     }
 
+    /*
+    * Method: state
+    * Pre: Space exists, called with .state
+    * Params: None
+    * Post: Getter for the values of the m_state of this Space object
+    */
     get state() {
         return this.m_state;
     }
 
+    /*
+    * Method: state
+    * Pre: Space exists, called with .state = {newState}
+    * Params: None
+    * Post: Setter for the values of the m_state. Modifies state based upon possible state values;
+    */
     set state(state) {
         switch (state) {
             case states.PLACED:
